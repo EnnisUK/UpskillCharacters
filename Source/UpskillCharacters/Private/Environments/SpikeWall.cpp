7b0d5fcaee201a17186f95d14e7b0e832ff56b26
Ennis <A0095091@live.tees.ac.uk>
2023-10-24 12:04:06 +0100

    SpikeWall BP

--- Source/UpskillCharacters/Private/Environments/SpikeWall.cpp
+++ Source/UpskillCharacters/Private/Environments/SpikeWall.cpp
@@ -9,12 +9,19 @@ ASpikeWall::ASpikeWall()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	m_wallMesh = CreateDefaultSubobject<UStaticMeshComponent>("WallMesh");
+	RootComponent = m_wallMesh;
+	m_spikeMesh = CreateDefaultSubobject<UStaticMeshComponent>("SpikeMesh");
+	m_spikeMesh->SetGenerateOverlapEvents(true);
+	m_spikeMesh->SetupAttachment(GetRootComponent());
 }
+
 
 // Called when the game starts or when spawned
 void ASpikeWall::BeginPlay()
 {
 	Super::BeginPlay();
+
 	
 }
 
@@ -25,3 +32,7 @@ void ASpikeWall::Tick(float DeltaTime)
 
 }
 
+
+void ASpikeWall::SpikeHitPlayer()
+{
+}
--- Source/UpskillCharacters/Public/Environments/SpikeWall.h
+++ Source/UpskillCharacters/Public/Environments/SpikeWall.h
@@ -10,14 +10,26 @@ UCLASS()
 class UPSKILLCHARACTERS_API ASpikeWall : public AActor
 {
 	GENERATED_BODY()
+
+		UPROPERTY(EditAnywhere)
+		UStaticMeshComponent* m_wallMesh;
+
+	UPROPERTY(EditAnywhere)
+		UStaticMeshComponent* m_spikeMesh;
 	
 public:	
 	// Sets default values for this actor's properties
 	ASpikeWall();
 
+	void SpikeHitPlayer();
+
+
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+
+	float m_damageAmount;
 
 public:	
 	// Called every frame
