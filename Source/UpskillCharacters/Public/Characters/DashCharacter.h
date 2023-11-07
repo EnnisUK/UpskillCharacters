c01409cb461ea2860f129e4f53d67e6716fa92d0
Ennis <A0095091@live.tees.ac.uk>
2023-11-07 18:59:06 +0000

    Idk

--- Source/UpskillCharacters/Public/Characters/DashCharacter.h
+++ Source/UpskillCharacters/Public/Characters/DashCharacter.h
@@ -23,6 +23,11 @@ public:
 	// Sets default values for this character's properties
 	ABurstHealCharacter();
 
+	void DashAbility();
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+	float SphereRadius = 200.f;
+
 
 protected:
 	// Called when the game starts or when spawned
@@ -33,20 +38,6 @@ protected:
 
 	/** Called for side to side input */
 	void MoveRight(float Value);
-
-
-
-	/**
-	 * Called via input to turn at a given rate.
-	 * @param Rate	This is a normalized rate, i.e. 1.0 means 100% of desired turn rate
-	 */
-	void TurnAtRate(float Rate);
-
-	/**
-	 * Called via input to turn look up/down at a given rate.
-	 * @param Rate	This is a normalized rate, i.e. 1.0 means 100% of desired turn rate
-	 */
-	void LookUpAtRate(float Rate);
 
 
 
